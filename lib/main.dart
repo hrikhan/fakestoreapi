fb1c6c78b6f5e36d1f9944d987e6345d5ad5eca6
hridoykhan51 <hridoykhan6786@gmail.com>
2025-02-12 17:24:33 +0600

    fake store api

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:api/shoping_homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const myapp());
+}
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+home: shoppinghomepage(),
+    );
+
+
+  }
+}
+
